5c30de51b8ace79a5b0cd20d023954d613858e0d
Abby Waddell <awaddell@sharphat.com>
2026-02-28 10:31:26 -0500

    Base Table no func

--- table.client/src/Table/Table.tsx
+++ table.client/src/Table/Table.tsx
@@ -1,41 +1,42 @@
-﻿import React from 'react';
-import { type TableProps, type Column } from '../Table.types.ts';
-function Table<T>({ data, columns, onRowClick, keyExtractor}: TableProps<T>) {
-  
+import type React from 'react';
+import { type TableProps, type Column } from './Table.types';
 
+function Table<T>({ data, columns, onRowClick, keyExtractor }: TableProps<T>) {
     return (
-            <table>
-                <thead>
-                    <tr>
-                        {columns.map((column: Column<T>) => {
+        <table>
+            <thead>
+                <tr>
+                    {columns.map((column: Column<T>) => (
+                        <th
+                            key={column.header}
+                            scope="col"
+                            style={{ width: column.width ?? 'auto' }}
+                        >
+                            {column.header}
+                        </th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {data?.map((row: T) => (
+                    <tr
+                        key={keyExtractor(row)}
+                        onClick={() => onRowClick?.(row)}
+                    >
+                        {columns.map((column, index) => {
+                            const value = row[column.key as keyof T];
                             return (
-                                <th key={column.header} scope='col' style={{ width: `${column.width ?? 'auto' }` }}>
-                                    {column.header}
-                                </th>
-                            )
+                                <td key={index}>
+                                    {column.render
+                                        ? column.render(value, row)
+                                        : (value as React.ReactNode)}
+                                </td>
+                            );
                         })}
                     </tr>
-                </thead>
-                <tbody>
-                    {data?.map((row: T) => {
-                        return (
-                            <tr key={keyExtractor(row)} onClick={() => onRowClick?.(row)}>
-                                {
-                                    columns.map((column, index2) => {
-                                        const value = row[column.key as keyof T];
-
-                                        return (
-                                            <td key={index2}>
-                                                {column.render ? column.render(value, row) : (value as React.ReactNode)}
-                                            </td>
-                                        )
-                                    })
-                                }
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
+                ))}
+            </tbody>
+        </table>
     );
 }
 
